8a66de572837796febfb25dc565e052f84de582f
Stephen Smith <stephen.smith@jointhedotsmr.com>
2020-01-21 15:52:04 +0000

    Add docblock tags for the facade's delegated methods

--- src/Facades/QueryString.php
+++ src/Facades/QueryString.php
@@ -5,6 +5,10 @@ namespace MeerkatMcr\SimpleQueryMemory\Facades;
 use Illuminate\Support\Facades\Facade;
 use MeerkatMcr\SimpleQueryMemory\Repositories\QueryDataRepository;
 
+/**
+ * @method static void forget(string $key)
+ * @method static array|null get(string $key, array $parameters = [])
+ */
 class QueryString extends Facade {
     protected static function getFacadeAccessor()
     {
